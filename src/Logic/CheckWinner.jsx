47d703d742782f6213b00b9503a88fda593be092
Gustavo Alzuru <gustavoalzuru7@gmail.com>
2023-06-26 20:03:20 -0400

    updating syntax

--- src/Logic/CheckWinner.jsx
+++ src/Logic/CheckWinner.jsx
@@ -1,15 +1,15 @@
-import { WINNER } from '../constants';
+import { WINNER } from '../constants'
 
 export const checkWinnerFrom = (boardToCheck) => {
-    for(const combos of WINNER){
-      const [a,b,c] = combos
-      if (
-        boardToCheck[a] &&
+  for (const combos of WINNER) {
+    const [a, b, c] = combos
+    if (
+      boardToCheck[a] &&
         boardToCheck[a] === boardToCheck[b] &&
         boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
+    ) {
+      return boardToCheck[a]
     }
-    }
-    return null
+  }
+  return null
 }
